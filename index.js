3d67717ebc94a3110c25705d72a4566bcf2e9635
Eric Guo <ericxguo@gmail.com>
2018-04-27 10:46:40 -0500

    added a bunch of stuff including sequelize

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const PORT = 1337;
 const app = express();
+const layout = require('./views/layout');
+const { db } = require('./models');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));  //current directory and add's /public
@@ -10,10 +12,14 @@ app.use(express.static(__dirname + '/public'));  //current directory and add's /
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req,res) => {
-  res.send('hello world');
+  res.send(layout(''));
 });
 
 app.listen(PORT , () => {
   console.log(`listening to port ${PORT}`);
+});
 
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
 });
